adf882faf78d7924d92a9b731fa08097c844b9bb
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-26 14:56:34 -0500

    Create Category entity

--- back/src/main/java/co/com/sofka/crud/category/Category.java
+++ back/src/main/java/co/com/sofka/crud/category/Category.java
@@ -1,4 +1,35 @@
 package co.com.sofka.crud.category;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @NotNull
+    @Size(min = 1)
+    String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
