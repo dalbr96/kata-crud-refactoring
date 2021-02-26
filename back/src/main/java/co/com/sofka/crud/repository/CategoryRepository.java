708448f4d6d1895833ca1f300bfb682576062da4
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-26 18:31:38 -0500

    Rearrange repository classes

--- back/src/main/java/co/com/sofka/crud/repository/CategoryRepository.java
+++ back/src/main/java/co/com/sofka/crud/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
-package co.com.sofka.crud.category;
+package co.com.sofka.crud.repository;
 
+import co.com.sofka.crud.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
