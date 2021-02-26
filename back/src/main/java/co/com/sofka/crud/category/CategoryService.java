669108a9a5aa76d25b4ae878fbbfb6f5604802d5
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-26 14:58:42 -0500

    Add - CategoryService

--- back/src/main/java/co/com/sofka/crud/category/CategoryService.java
+++ back/src/main/java/co/com/sofka/crud/category/CategoryService.java
@@ -1,4 +1,25 @@
 package co.com.sofka.crud.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Iterable<Category> list(){
+        return categoryRepository.findAll();
+    }
+
+    public Category save(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id){
+        categoryRepository.delete(get(id));
+    }
+
+    public Category get(Long id){
+        return categoryRepository.findById(id).orElseThrow();
+    }
 }
