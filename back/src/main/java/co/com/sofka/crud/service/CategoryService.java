265e4fc31e5106a5952bbd8fe8d28dc37e9e8764
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-28 12:51:03 -0500

    Add listInterfaz method

--- back/src/main/java/co/com/sofka/crud/service/CategoryService.java
+++ back/src/main/java/co/com/sofka/crud/service/CategoryService.java
@@ -1,7 +1,9 @@
 package co.com.sofka.crud.service;
 
+import co.com.sofka.crud.dto.InterfazDTO;
 import co.com.sofka.crud.model.Category;
 import co.com.sofka.crud.repository.CategoryRepository;
+import co.com.sofka.crud.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,9 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private TodoRepository todoRepository;
 
     public Iterable<Category> list(){
         return categoryRepository.findAll();
@@ -27,5 +32,15 @@ public class CategoryService {
 
     public Category get(Long id){
         return categoryRepository.findById(id).orElseThrow();
+    }
+
+    public InterfazDTO listInterfaz(){
+
+        InterfazDTO interfazDTO = new InterfazDTO();
+
+        interfazDTO.setCategories(list());
+        interfazDTO.setTodos(todoRepository.findAll());
+
+        return interfazDTO;
     }
 }
